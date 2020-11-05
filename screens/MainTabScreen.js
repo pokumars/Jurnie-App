86e76969ca6cdc43812bc803cdde5855ebae3348
Nguyen Nguyen <nguyen.nguyen@punosmobile.com>
2020-11-05 15:43:25 +0200

    now move the layout of the BottomTabBar to components/TabBar

--- screens/MainTabScreen.js
+++ screens/MainTabScreen.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import FastImage from 'react-native-fast-image';
 
 import HomeScreen from './HomeScreen';
 import MyTripsScreen from './MyTripsScreen';
@@ -10,7 +8,7 @@ import LeaderboardScreen from './LeaderboardScreen';
 import ProfileScreen from './ProfileScreen';
 
 import { COLOR, NAVIGATION_ROUTE } from '../constants';
-import { tabBarImageNameFormat } from '../helpers';
+import CustomizedTabBar from '../components/TabBar';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,35 +16,7 @@ const MainTabScreen = ({ navigation }) => {
   return (
     <Tab.Navigator
       initialRouteName={NAVIGATION_ROUTE.HOME}
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let imageSource;
-
-          switch (route.name) {
-            case NAVIGATION_ROUTE.HOME:
-              imageSource = focused ? require('../assets/icons/homeActive.png') : require('../assets/icons/home.png')
-              break;
-            case NAVIGATION_ROUTE.MY_TRIPS:
-              imageSource = focused ? require('../assets/icons/myTripsActive.png') : require('../assets/icons/myTrips.png')
-              break;
-            case NAVIGATION_ROUTE.LEADERBOARD:
-              imageSource = focused ? require('../assets/icons/leaderboardActive.png') : require('../assets/icons/leaderboard.png')
-              break;
-            case NAVIGATION_ROUTE.PROFILE:
-              imageSource = focused ? require('../assets/icons/profileActive.png') : require('../assets/icons/profile.png')
-              break;
-            default:
-              imageSource = null;
-          }
-
-          return (
-            <FastImage
-              source={imageSource}
-              style={styles.tabBarIcon}
-            />
-          );
-        },
-      })}
+      tabBar={props => <CustomizedTabBar {...props} />}
       tabBarOptions={{
         activeTintColor: COLOR.STEEL_BLUE,
         inactiveTintColor: 'gray',
@@ -83,12 +53,5 @@ const MainTabScreen = ({ navigation }) => {
     </Tab.Navigator>
     )
 };
-
-const styles = StyleSheet.create({
-  tabBarIcon: {
-    width: 24,
-    height: 24,
-  }
-})
 
 export default MainTabScreen;
