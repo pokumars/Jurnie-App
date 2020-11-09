269ed28c437a796b97929445b810d2469e537718
oheneba <ohenebakob@hotmail.com>
2020-11-09 21:40:10 +0200

    add profile picture and put in some dummy condition for when you have profile pics

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -5,8 +5,12 @@ import {StackActions} from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
 import {createStackNavigator} from '@react-navigation/stack';
 import IconTextBorderlessBtn from '../components/Profile/IconTextBorderlessBtn';
+import color from '../constants/color';
 
 const ProfileScreen = ({navigation}) => {
+  //TODO: if a user has a profile pic, use that else use the profile icon
+  const profilePicUrl = 'https://ohe-test-image-upload-1.s3.amazonaws.com/44e97045-fd97-4882-8ed1-942934d6bee4.png'
+  const profilePicPlaceholderTruth = false
   const Sign_out = () => {
     /*auth()
       .signOut()
@@ -30,6 +34,13 @@ const ProfileScreen = ({navigation}) => {
          onPress={() => Sign_out()}
         />
       </View>
+      <View style={{width: '100%', alignItems: 'center'}} >
+        <View style={styles.profilePicContainer}>
+          <Image style={styles.profilePic} source={profilePicPlaceholderTruth? {uri: profilePic}:require('../assets/icons/profile.png')} />
+        </View>
+      </View>
+
+
       {/*
       <View style={styles.main}>
         <Text>ProfileScreen,,{auth().currentUser.email}</Text>
@@ -46,7 +57,10 @@ const ProfileScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 15
+    padding: 15,
+    justifyContent: 'center',
+    
+    
   },
   main: {
     alignItems: 'center',
@@ -55,8 +69,23 @@ const styles = StyleSheet.create({
   settingsLogoutContainer:{
     flexDirection: 'row',
     width: '100%',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+
+    padding: 5
   },
+  profilePicContainer: {
+    height: 150,
+    width: 150,
+
+    borderRadius: 75,
+    borderWidth: 1,
+    borderColor: color.STEEL_BLUE,
+    overflow: "hidden"
+  },
+  profilePic: {
+    width:'100%',
+    height: '100%'
+  }
 });
 
 export default ProfileScreen;
