3198b965e3ce07114235d99877654a3992acc6ca
oheneba <ohenebakob@hotmail.com>
2020-11-24 00:14:00 +0200

    create helper file for survey; add transport modes in their own array

---
+++ helpers/TmdTransportModes.js
@@ -0,0 +1,6 @@
+export const transportModes = [
+  'non-motorized/bicycle', 'non-motorized/pedestrian/walk', 
+'non-motorized/pedestrian/run', 'motorized/road/car',
+'motorized/road/bus', 'motorized/rail/tram', 'motorized/rail/train',
+ 'motorized/rail/metro', 'motorized/air/plane'
+]
