2f3440474b1f6fb73fccb0f7aae571b34ae3c860
oheneba <ohenebakob@hotmail.com>
2020-12-01 18:49:54 +0200

    create a named tuple3 for the feedback amount milestones

---
+++ helpers/badgeWinning.js
@@ -0,0 +1,16 @@
+import { determineBadgeIcon } from"./determineAsset"
+
+// feedbackAmountMilestones - we are using named tuple3 here. Javascript doesnt have tuples so you can mock or mimic tuples with arrays or objects
+// for more info on tuple https://www.tutorialspoint.com/scala_collections/scala_collections_tuple.htm 
+// or https://medium.com/better-programming/tuples-in-javascript-57ede9b1c9d2
+export const feedbackAmountMilestones = [
+  { score: 0, badgeImage: determineBadgeIcon('trophy'), text: 'the test run' },
+  { score: 1, badgeImage: determineBadgeIcon('trophy'), text: 'your 1st feedback milestone badge' },
+  { score: 5, badgeImage: determineBadgeIcon('trophy'), text: "a badge for your 5th feedback"},
+  { score: 10, badgeImage: determineBadgeIcon('trophy'), text: "a badge for your 10th feedback"},
+  { score: 20, badgeImage: determineBadgeIcon('trophy'), text: "a badge for your 20th feedback"},
+  { score: 50, badgeImage: determineBadgeIcon('trophy'), text: "a badge for your 50th feedback"},
+  { score: 100, badgeImage: determineBadgeIcon('trophy'), text: "a badge for your 100th feedback"},
+];
+
+
