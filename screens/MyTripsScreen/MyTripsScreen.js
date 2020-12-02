e931c01c8fb60f42f6ea06f38e2d056d1b626c12
Nguyen Nguyen <nguyen.nguyen@punosmobile.com>
2020-12-02 07:27:35 +0200

    complete the features

--- screens/MyTripsScreen/MyTripsScreen.js
+++ screens/MyTripsScreen/MyTripsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Text,
   View,
@@ -25,91 +25,20 @@ const MyTripsScreen = ({ navigation }) => {
   const [currentTrip, setcurrentTrip] = useState([]);
   const [selectedId, setselectedId] = useState(null);
 
-  const Item = ({ title, start, mode, id, feed, onPress }) => (
-    <TouchableOpacity onPress={onPress} style={styles.item}>
-      <Text style={{ fontWeight: 'bold' }}>{start}</Text>
-      <Text>{GetMode(mode)}</Text>
-      <Text style={{ fontWeight: 'bold' }}>{title}</Text>
-      {feed == false ? (
-        <TouchableOpacity
-          style={{ backgroundColor: '#DDDDDD', padding: 10 }}
-          onPress={() =>
-            navigation.navigate('Questionnaire', {
-              paramKey: id,
-            })
-          }>
-          <Text>FeedBack</Text>
-        </TouchableOpacity>
-      ) : (
-        <TouchableOpacity
-          style={{ backgroundColor: '#DDDDDD', padding: 10 }}
-          onPress={() =>
-            navigation.navigate('Questionnaire', {
-              paramKey: id,
-            })
-          }>
-          <Text>Update Feed</Text>
-        </TouchableOpacity>
-      )}
-    </TouchableOpacity>
-  );
-
-  const GetMode = (activity) => {
-    switch (activity) {
-      case 'stationary':
-        return 'stationary';
-
-      case 'non-motorized/bicycle':
-        return 'bicycle';
-
-      case 'non-motorized/pedestrian':
-        return 'pedestrian';
-
-      case 'non-motorized/pedestrian/walk':
-        return 'walk';
-
-      case 'non-motorized/pedestrian/run':
-        return 'run';
-
-      case 'motorized/road/car':
-        return 'car';
-
-      case 'motorized/road/bus':
-        return 'bus';
-
-      case 'motorized/rail':
-        return 'rail';
-
-      case 'motorized/rail/tram':
-        return 'tram';
-
-      case 'motorized/rail/train':
-        return 'train';
-
-      case 'motorized/rail/metro':
-        return 'metro';
-
-      case 'motorized/air/plane':
-        return 'plain';
-
-      default:
-        return 'unknown';
-    }
-  };
+  useEffect(() => {
+    firestore()
+      .collection('users')
+      .doc(auth().currentUser.email)
+      .collection('trips')
+      .where('datefortrips', '==', moment(selectedDate).format('DD/MM/YYYY'))
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => doc.data());
+        setcurrentTrip(data);
+      });
+  }, [selectedDate]);
 
   const renderItem = ({ item }) => (
-    // <Item
-    //   title={item.timeEnd}
-    //   start={item.timestart}
-    //   mode={item.activityType}
-    //   id={item.id}
-    //   feed={item.feedGiven}
-    //   onPress={() =>
-    //     navigation.navigate('Detailed', {
-    //       paramKey: item.id,
-    //     })
-    //   }
-    // />
     <TripCard
       endingTime={item.timeEnd}
       feed={item.feedGiven}
@@ -133,29 +62,12 @@ const MyTripsScreen = ({ navigation }) => {
     });
   };
 
-  const GetCurrent = () => {
-    firestore()
-      .collection('users')
-      .doc(auth().currentUser.email)
-      .collection('trips')
-      .where('datefortrips', '==', moment(selectedDate).format('DD/MM/YYYY'))
-      .get()
-      .then((querySnapshot) => {
-        const data = querySnapshot.docs.map((doc) => doc.data());
-        // console.log(data);
-
-        setcurrentTrip(data);
-        console.log('kkkkk', data);
-      });
-  };
-
   return (
     <ScreenContainer>
       <WeekCalendar
         currentDate={new Date()}
         {...{ selectedDate, setSelectedDate }}
       />
-      <Button title="get" onPress={() => GetCurrent()} />
       {currentTrip.length !== 0 ? (
         <FlatList
           data={currentTrip}
@@ -170,44 +82,10 @@ const MyTripsScreen = ({ navigation }) => {
   );
 };
 
-/*const MyTrips = () => (
-  <>
-    <SubtitleText>MY TRIPS</SubtitleText>
-    <FlatList
-      data={testTrips}
-      keyExtractor={(item, index) => `Trip-${item.origin}-${item.destination}-${index}`}
-      {...{ renderItem }}
-    />
-  </>
-);
-
-const renderItem = ({ item }) => (
-  <TripCard
-    destination={item.destination}
-    meansOfTransport={item.meansOfTransport}
-    origin={item.origin}
-  />
-);*/
-
 const ScreenContainer = styled.View`
   ${DefaultContainer};
   flex: 1;
   padding-top: ${StackM}px;
 `;
-
-const styles = StyleSheet.create({
-  item: {
-    backgroundColor: '#f9c2ff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    fontSize: 20,
-  },
-});
 
 export default MyTripsScreen;
