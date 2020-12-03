598d161f35cc1319f7777c504cb13324d5995342
oheneba <ohenebakob@hotmail.com>
2020-12-04 01:05:36 +0200

    highlight selected button in booleanUnsureResponse of questionaire

--- components/SurveyComponents/BooleanUnsureResponse.js
+++ components/SurveyComponents/BooleanUnsureResponse.js
@@ -1,6 +1,6 @@
 // import libraries
-import React, { useEffect } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Button, StyleSheet, Pressable, Text } from 'react-native';
 import color from '../../constants/color';
 import globalStyles from '../../constants/globalStyle';
 
@@ -8,59 +8,88 @@ const BooleanUnsureResponse = (props) => {
   useEffect(() => {
     props.setMediaOrDone(null);
   });
+  // selectedBtn is to determine which button is selected so we can highlight it.
+  const [selectedBtnText, setSelectedBtnText] = useState(null);
+  const highlightSelection = (btnText) => {
+    if (btnText === selectedBtn) {
+      return {};
+    }
+  };
 
   return (
     <View style={globalStyles.responderViewContainer}>
       <View style={styles.buttonsContainer}>
-        <View style={styles.buttonView}>
-          <Button
-            title="yes"
-            color={color.STEEL_BLUE}
-            onPress={() => {
-              // show btn that says next
-              props.setAnswered(true);
-              props.setAnswer('yes');
-              console.log('yes clicked');
-            }}
-          />
-        </View>
-        <View style={styles.buttonView}>
-          <Button
-            title="no"
-            color={color.STEEL_BLUE}
-            onPress={() => {
-              // show btn that says next
-              props.setAnswered(true);
-              props.setAnswer('no');
-              console.log('no clicked');
-            }}
-          />
-        </View>
-        <View style={styles.buttonView}>
-          <Button
-            title="not sure"
-            color={color.RAJAH}
-            onPress={() => {
-              // show btn that says next
-              props.setAnswered(true);
-              props.setAnswer('not sure');
-              console.log('not sure clicked');
-            }}
-          />
-        </View>
+        <Pressable
+          style={[
+            styles.buttonView,
+            { backgroundColor: selectedBtnText === 'yes' ? color.STEEL_BLUE : 'white' },
+          ]}
+          onPress={() => {
+            props.setAnswered(true); // shows btn that says next
+            props.setAnswer('yes'); // set answer in Questionnaire
+            setSelectedBtnText('yes') // so we know which button to highlight as selected
+            console.log('yes clicked');
+          }}>
+          <Text style={[styles.btnText, { color: selectedBtnText === 'yes' ? 'white' : 'black' }]}>
+            Yes
+          </Text>
+        </Pressable>
+
+        <Pressable
+          style={[
+            styles.buttonView,
+            { backgroundColor: selectedBtnText === 'no' ? color.STEEL_BLUE : 'white' },
+          ]}          onPress={() => {
+            props.setAnswered(true); // shows btn that says next
+            props.setAnswer('no'); // set answer in Questionnaire
+            setSelectedBtnText('no') // so we know which button to highlight as selected
+            console.log('no clicked');
+          }}>
+          <Text style={[styles.btnText, { color: selectedBtnText === 'no' ? 'white' : 'black' }]}>
+            No
+          </Text>
+        </Pressable>
+
+        <Pressable
+          style={[
+            styles.buttonView,
+            // if this btn is selected, give the button background
+            { backgroundColor: selectedBtnText === 'not sure' ? color.STEEL_BLUE : 'white' },
+          ]}
+          onPress={() => {
+            props.setAnswered(true); // shows btn that says next
+            props.setAnswer('not sure'); // set answer in Questionnaire
+            setSelectedBtnText('not sure'); // so we know which button to highlight as selected
+            console.log('not sure clicked');
+          }}>
+          <Text style={[styles.btnText, { color: selectedBtnText === 'not sure' ? 'white' : 'black' }]}>
+            Not Sure
+          </Text>
+        </Pressable>
       </View>
     </View>
   );
 };
+const btnHeight = 35;
 
 const styles = StyleSheet.create({
   buttonsContainer: {
     flexDirection: 'row',
     width: '90%',
     justifyContent: 'space-around',
+    padding: 5,
   },
   buttonView: {
     width: '30%',
+    height: btnHeight,
+    borderWidth: 2,
+    borderColor: color.STEEL_BLUE,
+    borderRadius: btnHeight / 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    color: 'black',
   },
 });
 
